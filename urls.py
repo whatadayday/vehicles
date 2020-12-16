4a5f969c977cef01193173be10a0f17e81648633
Evgeny Ivanenko <Evgeny.Ivanenko@dataart.com>
2020-12-16 14:25:23 +0200

    test commit

--- urls.py
+++ urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import position, find_vehicles
-#import .views
+#from .views import position, find_vehicles
+from .views import *
 
 
 urlpatterns = {
-    path('<int:vehicle_id>/position', position, name="store_position"),
     path('', find_vehicles, name="find_vehicles"),
+    path('items/', items, name="items"),
+    path('<int:vehicle_id>/position', position, name="store_position"),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
